97112dae278151b24b3d28265fe37ff2d8f318e0
kory <62596617+korylee@users.noreply.github.com>
2026-06-16 18:49:41 +0800

    feat(utils): add dom utilities

--- packages/utils/src/solid/dom.ts
+++ packages/utils/src/solid/dom.ts
@@ -1,18 +1,16 @@
 import { JSX } from "solid-js";
 import { isServer } from "solid-js/web";
 
-const isServerElement = <E extends Element>(item: JSX.Element): item is E =>
+const isServerElement = <E extends Element>(item: JSX.Element | E): item is E =>
   item != null && typeof item === "object" && "t" in item;
 
 export const genIsElement =
   <E extends abstract new (...args: any[]) => Element>(ElementConstructor: E) =>
-  (item: JSX.Element | InstanceType<E>): item is InstanceType<E> => {
-    if (isServer) {
-      return isServerElement<InstanceType<E>>(item);
-    }
-    return item instanceof ElementConstructor;
-  };
+  (item: JSX.Element | InstanceType<E>): item is InstanceType<E> =>
+    item instanceof ElementConstructor;
 
-export const isElement = genIsElement(Element);
+export const isElement = isServer ? isServerElement : genIsElement(Element);
 
-export const isHTMLElement = genIsElement(HTMLElement);
+export const isHTMLElement = isServer
+  ? isServerElement<HTMLElement>
+  : genIsElement(HTMLElement);
